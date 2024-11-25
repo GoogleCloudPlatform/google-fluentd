816a2b40e17dd804a0cc1af97a784baa20e77e57
Francisco Valente Castro <1435136+franciscovalentecastro@users.noreply.github.com>
2024-11-25 17:26:19 -0500

    Update `fluent-plugin-(detect-exceptions|kubernetes_metadata_filter)` and related gem versions. (#445)

--- plugin_gems.rb
+++ plugin_gems.rb
@@ -2,7 +2,7 @@ dir 'plugin_gems'
 # The order of these dependencies matters due to how Ruby pulls dependencies.
 download "httpclient", "2.8.3"
 download "uuidtools", "2.1.5"
-download "jmespath", "1.4.0"
+download "jmespath", "1.6.2"
 download "aws-partitions", "1.149.0"
 download "aws-sigv4", "1.1.0"
 download "aws-sdk-core", "3.48.3"
@@ -17,7 +17,7 @@ download "fluent-plugin-rewrite-tag-filter", "2.2.0"
 # The release candidate build will overwrite this gem with a gem version that is built from the master branch
 # later in the build pipeline.
 download "fluent-plugin-google-cloud", "0.13.2"
-download "fluent-plugin-detect-exceptions", "0.0.15"
+download "fluent-plugin-detect-exceptions", "0.0.16"
 # Keep this version compatible with
 # https://github.com/fluent/fluent-plugin-prometheus/blob/master/fluent-plugin-prometheus.gemspec
 download "prometheus-client", "0.9.0"
@@ -26,7 +26,7 @@ download "fluent-plugin-prometheus", "1.4.0"
 download "fluent-plugin-multi-format-parser", "1.0.0"
 download "fluent-plugin-record-reformer", "0.9.1"
 download "fluent-plugin-record-modifier", "2.0.1"
-download "fluent-plugin-kubernetes_metadata_filter", "2.5.2"
+download "fluent-plugin-kubernetes_metadata_filter", "3.5.1"
 download "systemd-journal", "1.4.2"
 download "fluent-plugin-systemd", "1.0.5"
 if windows?
