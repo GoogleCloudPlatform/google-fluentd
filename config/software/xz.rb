db99e114b477ce5d25d29c2e11b602362bd194a6
Lujie Duan <lujie.duan@usask.ca>
2025-07-18 13:19:54 -0400

    Update the xz package to download from GitHub (#452)

--- config/software/xz.rb
+++ config/software/xz.rb
@@ -1,5 +1,9 @@
 name "xz"
-default_version "5.2.3"
+default_version "5.8.1"
+
+version "5.8.1" do
+  source md5: "209cb25608ab0841f68df4d50d0faa22"
+end
 
 version "5.2.3" do
   source md5: "ef68674fb47a8b8e741b34e429d86e9d"
@@ -13,7 +17,8 @@ version "5.2.1" do
   source md5: "3e44c766c3fb4f19e348e646fcd5778a"
 end
 
-source url: "https://storage.googleapis.com/stackdriver-fluentd-vendor-storage/xz-#{version}.tar.gz"
+# Example: https://github.com/tukaani-project/xz/releases/download/v5.8.1/xz-5.8.1.tar.gz
+source url: "https://github.com/tukaani-project/xz/releases/download/v#{version}/xz-#{version}.tar.gz"
 
 relative_path "xz-#{version}"
 
